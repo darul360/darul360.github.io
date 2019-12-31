7071cf4d8d465fcac59c9f605d4bccd0143888db
Łukasz Darul <210159@edu.p.lodz.pl>
2019-12-31 11:58:29 +0100

    bvhgjhj

--- picRotate.js
+++ picRotate.js
@@ -10,34 +10,56 @@ function MouseOut(image) {
 
 
 function titleOver(text) {
-  text.setAttribute("style", "font-size: 35px;")
+  text.setAttribute("style", "font-size: 1.4vw;")
 }
 
 function titleOut(text) {
-  text.setAttribute("style", "font-size: 30px;")
+  text.setAttribute("style", "font-size: 1.2vw;")
 }
 
 function oneOver(body,text){
   body.setAttribute("style", "background-color: rgb(246, 218, 240);")
-  text.setAttribute("style", "font-size: 35px;")
+  /*text.setAttribute("style", "font-size: 1.4vw;")*/
 }
 
 function twoOver(body,text){
   body.setAttribute("style", "background-color: rgb(241, 218, 213);")
-  text.setAttribute("style", "font-size: 35px;")
+  /*text.setAttribute("style", "font-size: 1.4vw;")*/
 }
 
 function threeOver(body,text){
   body.setAttribute("style", "background-color: rgb(228, 241, 227);")
-  text.setAttribute("style", "font-size: 35px;")
+  /*text.setAttribute("style", "font-size: 1.4vw;")*/
 }
 
 function fourOver(body,text){
   body.setAttribute("style", "background-color: rgb(250, 244, 226);")
-  text.setAttribute("style", "font-size: 35px;")
+  /*text.setAttribute("style", "font-size: 1.4vw;")*/
 }
 
 function oneOut(body,text){
   body.setAttribute("style", "background-color: rgb(228, 234, 241);")
-  text.setAttribute("style", "font-size: 30px;")
+  /*text.setAttribute("style", "font-size: 1.2vw;")*/
 }
+
+/*
+function typeWriter(text){
+    var i=0;
+    var textSrc = text.textContent;
+    text.style.color = 'white';
+    text.charAt(0).style.color = 'black';
+    var array = text.innerHTML.split();
+    text[0].style.color = 'black';
+    function func(){
+        if(i < text.textContent.length)
+        {
+            text.charAt(i).style.color = 'black';
+            i++;
+            setTimeout(func,50);
+        }
+        
+    }
+    func();
+
+}
+*/
