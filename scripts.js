d35ae3d416ecaab238fc6a2c14bd80162defa514
Łukasz <210159@edu.p.lodz.pl>
2020-03-03 01:52:13 +0100

    add

--- scripts.js
+++ scripts.js
@@ -1,17 +1,90 @@
-function disableElements1(){
-  var nodes = document.getElementById("subject2");
-  if(window.getComputedStyle(nodes).display === "inline"){
-  nodes.style.display = 'none';
+function disableElements0(){
+
+
+  var nodes2 = document.getElementById("subject0_1");
+  if(window.getComputedStyle(nodes2).display === "inline"){
+  nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown0").src = "arrows2.png";
+
   }
   else{
-    nodes.style.display = 'inline';
+    nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown0").src = "arrows.png";
   }
+}
+
+
+function disableElements1(){
+
 
   var nodes2 = document.getElementById("subject1_1");
   if(window.getComputedStyle(nodes2).display === "inline"){
   nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown1").src = "arrows2.png";
+
   }
   else{
     nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown1").src = "arrows.png";
+  }
+}
+
+function disableElements2(){
+
+
+  var nodes2 = document.getElementById("subject2_1");
+  if(window.getComputedStyle(nodes2).display === "inline"){
+  nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown2").src = "arrows2.png";
+
+  }
+  else{
+    nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown2").src = "arrows.png";
+  }
+}
+
+function disableElements3 (){
+
+
+  var nodes2 = document.getElementById("subject3_1");
+  if(window.getComputedStyle(nodes2).display === "inline"){
+  nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown3").src = "arrows2.png";
+
+  }
+  else{
+    nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown3").src = "arrows.png";
+  }
+}
+
+function disableElements4 (){
+
+
+  var nodes2 = document.getElementById("subject4_1");
+  if(window.getComputedStyle(nodes2).display === "inline"){
+  nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown4").src = "arrows2.png";
+
+  }
+  else{
+    nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown4").src = "arrows.png";
+  }
+}
+
+function disableElements5 (){
+
+
+  var nodes2 = document.getElementById("subject5_1");
+  if(window.getComputedStyle(nodes2).display === "inline"){
+  nodes2.style.display = 'none';
+  var imageClose = document.getElementById("arrowDown5").src = "arrows2.png";
+
+  }
+  else{
+    nodes2.style.display = 'inline';
+    var imageOpen= document.getElementById("arrowDown5").src = "arrows.png";
   }
 }
